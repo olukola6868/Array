933026b8cd5217d5f31ab70a0c37e09e0a47233f
olukola6868 <akanbib29@gmail.com>
2022-10-11 09:13:16 -1200

    My array assessment

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ARRAY
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Hello World!");
+           Console.WriteLine("ENter the length of the array");
+           int length = int.Parse(Console.ReadLine());
+           int[]array = new int[length];
+           Console.WriteLine("Enter the sorted array number");
+           int number = int.Parse(Console.ReadLine());
+           for(int i =0;i<array.Length;i++)
+           {
+               Console.WriteLine("Enter your number");
+               array[i] = int.Parse(Console.ReadLine());
+           }
+           for(int i = 1;i < array.Length;i++)
+           {
+               if(array[i] == number)
+               {
+                   Console.WriteLine("The number is available");
+               }
+               else
+               {
+                   Console.WriteLine(-1);
+               }
+           }
+
+
+        }
+    }
+}
